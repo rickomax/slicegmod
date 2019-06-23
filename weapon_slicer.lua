a9773a0031eadeddb2b0b89c162f4a56bc34382c
Ricardo Reis <rickomax@hotmail.com>
2019-06-22 21:47:55 -0300

    Update weapon_slicer.lua

--- weapon_slicer.lua
+++ weapon_slicer.lua
@@ -26,22 +26,104 @@ SWEP.Secondary.Ammo = "none"
 
 SWEP.DrawAmmo = false
 
+local cachedMaterials = {}
+
 function SWEP:Initialize()
 	self:SetHoldType( "slam" )
 end
 
 function SWEP:SetupDataTables()
-	self:NetworkVar("Int", 0, "SlicedIndexA")
-	self:NetworkVar("Int", 0, "SlicedIndexB")
-	self:NetworkVar("Vector", 0, "TPlanePosition")
-	self:NetworkVar("Float", 0.0, "TPlaneDistance")
+	self:NetworkVar("Entity", 1, "SideEntity")
+	self:NetworkVar("Entity", 2, "NotSideEntity")
+	self:NetworkVar("Vector", 3, "SlicePlaneDirection")
+	self:NetworkVar("Float",  4, "SlicePlaneDistance")
+end
+
+function SWEP:PrimaryAttack()
+	if (game.SinglePlayer()) then 
+		self:CallOnClient("PrimaryAttack")
+	end
+	if (!CLIENT) then 
+		if (self.Owner:IsPlayer()) then
+			self.Owner:LagCompensation(true)
+		end
+		local startpos = self.Owner:GetShootPos()
+		local endpos = self.Owner:GetShootPos() + self.Owner:GetAimVector() * 100
+		local trace = util.TraceLine({
+			start = startpos,
+			endpos = endpos,
+			filter = self.Owner
+		})
+		if (self.Owner:IsPlayer()) then
+			self.Owner:LagCompensation(false)
+		end
+		if (IsValid(trace.Entity)) then
+			local sideEntity = trace.Entity
+			sidePhysObject = sideEntity:GetPhysicsObject()
+			sidePhysMesh = sidePhysObject:GetMesh()
+			if (sidePhysMesh) then
+				local planePosition = trace.HitPos
+				local planeDirection = self.Owner:GetAngles():Right()
+				local planeDistance = GetPlaneDistance(planeDirection, planePosition)
+				local sideMesh, notSideMesh = SplitMesh(sidePhysMesh, sidePhysObject:GetPositionMatrix(), planeDirection, planeDistance)	
+				sideEntity:PhysicsFromMesh(sideMesh)
+				sidePhysObject = sideEntity:GetPhysicsObject()
+				sidePhysObject:ApplyForceCenter(-planeDirection * sidePhysObject:GetMass())
+				local notSideEntity = ents.Create(sideEntity:GetClass())
+				notSideEntity:SetModel(sideEntity:GetModel())
+				notSideEntity:SetPos(sideEntity:GetPos())
+				notSideEntity:SetAngles(sideEntity:GetAngles())
+				notSideEntity:Spawn()
+				notSideEntity:PhysicsFromMesh(notSideMesh)	
+				local notSidePhysObject = notSideEntity:GetPhysicsObject()
+				notSidePhysObject:ApplyForceCenter(planeDirection * notSidePhysObject:GetMass())
+				self:SetSlicePlaneDirection(planeDirection)
+				self:SetSlicePlaneDistance(planeDistance)
+				self:SetSideEntity(sideEntity)
+				self:SetNotSideEntity(notSideEntity)
+			end
+		end
+	else
+		local sideEntity = self:GetSideEntity()
+		if (sideEntity) then
+			local planeDirection = self:GetSlicePlaneDirection()
+			local planeDistance = self:GetSlicePlaneDistance()
+			local matrix = sideEntity:GetWorldTransformMatrix()
+			local model = sideEntity:GetModel()	
+			local meshes = sideEntity.meshes or util.GetModelMeshes(model)
+			local notSideEntity = self:GetNotSideEntity()
+			sideEntity.meshes, notSideEntity.meshes = SplitMultiMesh(meshes, matrix, planeDirection, planeDistance)
+			sideEntity.RenderOverride = function()
+				RenderSlice(sideEntity)
+			end
+			notSideEntity.RenderOverride = function()
+				RenderSlice(notSideEntity)
+			end
+		end
+	end
+end
+
+function SWEP:SecondaryAttack()
+
+end
+
+function SWEP:OnRemove()
+
+end
+
+function SWEP:Holster()
+	return true
+end
+
+function SWEP:CustomAmmoDisplay()
+	return self.AmmoDisplay
 end
 
 function IntersectRayWithPlane(a, b, planeNormal, planeDistance)
 	local enter, point = PlaneRaycast((b-a):GetNormalized(), a, planeNormal, planeDistance)
 	if (enter > 0.0) then
-		--local distance = a:Distance(point) / a:Distance(b)
-		return point, 0 --TODo: distance (used for interpolation)
+		local distance = a:Distance(point) / a:Distance(b)
+		return point, distance 
 	end
 	return nil, 0
 end
@@ -67,6 +149,8 @@ end
 function SplitMesh(mesh, matrix, planeDirection, planeDistance)
 	local sideMesh = {}
 	local notSideMesh = {}
+	local innerTriangles = {}
+	local innerPlaneTriangles = {}
 	local invMatrix = matrix:GetInverse()
 	for index = 1, #mesh, 3 do
 		local a = mesh[index]
@@ -130,120 +214,100 @@ function SplitMesh(mesh, matrix, planeDirection, planeDistance)
 				index1 = firstSideIndex
 			end
 			local points = {a,b,c,a,b,c} --dirty hack
-			local point1 = points[index1]
-			local point2 = points[index1+1]
-			local point3 = points[index1+2]
-			local point1Pos = matrix * point1.pos
-			local point2Pos = matrix * point2.pos
-			local point3Pos = matrix * point3.pos
-			local point12, point4Dist = IntersectRayWithPlane(point1Pos, point2Pos, planeDirection, planeDistance)
-			local point31, point5Dist = IntersectRayWithPlane(point3Pos, point1Pos, planeDirection, planeDistance)
+			local vertex1 = points[index1]
+			local vertex2 = points[index1+1]
+			local vertex3 = points[index1+2]
+			local point1 = matrix * vertex1.pos
+			local point2 = matrix * vertex2.pos
+			local point3 = matrix * vertex3.pos
+			local point12, point12Dist = IntersectRayWithPlane(point1, point2, planeDirection, planeDistance)
+			local point31, point31Dist = IntersectRayWithPlane(point3, point1, planeDirection, planeDistance)
 			if (point12 && point31) then
-				table.insert(triangleMesh, point1)
-				table.insert(triangleMesh, {pos=invMatrix*point12})
-				table.insert(triangleMesh, {pos=invMatrix*point31})
-				
-				table.insert(quadMesh, {pos=invMatrix*point12})
-				table.insert(quadMesh, point2)
-				table.insert(quadMesh, point3)
-				
-				table.insert(quadMesh, point3)
-				table.insert(quadMesh, {pos=invMatrix*point31})
-				table.insert(quadMesh, {pos=invMatrix*point12})
+				local vertex12 = {pos = invMatrix * point12}
+				if (vertex1.normal and vertex2.normal) then
+					vertex12.normal = LerpVector(point12Dist, vertex1.normal, vertex2.normal)
+				end
+				if (vertex1.tangent and vertex2.tangent) then
+					vertex12.tangent = LerpVector(point12Dist, vertex1.tangent, vertex2.tangent)
+				end
+				if (vertex1.binormal and vertex2.binormal) then
+					vertex12.binormal = LerpVector(point12Dist, vertex1.binormal, vertex2.binormal)
+				end
+				if (vertex1.u and vertex2.u) then
+					vertex12.u = Lerp(point12Dist, vertex1.u, vertex2.u)
+				end
+				if (vertex1.v and vertex2.v) then
+					vertex12.v = Lerp(point12Dist, vertex1.v, vertex2.v)
+				end
+				local vertex31 = {pos = invMatrix * point31}
+				if (vertex3.normal and vertex1.normal) then
+					vertex31.normal = LerpVector(point31Dist, vertex3.normal, vertex1.normal)
+				end
+				if (vertex3.tangent and vertex1.tangent) then
+					vertex31.tangent = LerpVector(point31Dist, vertex3.tangent, vertex1.tangent)
+				end
+				if (vertex3.binormal and vertex1.binormal) then
+					vertex31.binormal = LerpVector(point31Dist, vertex3.binormal, vertex1.binormal)
+				end
+				if (vertex3.u and vertex1.u) then
+					vertex31.u = Lerp(point31Dist, vertex3.u, vertex1.u)
+				end
+				if (vertex3.v and vertex1.v) then
+					vertex31.v = Lerp(point31Dist, vertex3.v, vertex1.v)
+				end
+				table.insert(triangleMesh, vertex1)
+				table.insert(triangleMesh, vertex12)
+				table.insert(triangleMesh, vertex31)
+				table.insert(quadMesh, vertex12)
+				table.insert(quadMesh, vertex2)
+				table.insert(quadMesh, vertex3)
+				table.insert(quadMesh, vertex3)
+				table.insert(quadMesh, vertex31)
+				table.insert(quadMesh, vertex12)
 			end
 		end
-	end
-	for index = 1, #sideMesh, 3 do
-		local a = sideMesh[index]
-		local b = sideMesh[index+1]
-		local c = sideMesh[index+2]
-		local aPos = matrix * a.pos
-		local bPos = matrix * b.pos
-		local cPos = matrix * c.pos
-		debugoverlay.Triangle(aPos,bPos,cPos,20,Color(255,255,0),true)
-		debugoverlay.Line(aPos, bPos, 20, Color(0,0,0),true)
-		debugoverlay.Line(bPos, cPos, 20, Color(0,0,0),true)
-		debugoverlay.Line(cPos, aPos, 20, Color(0,0,0),true)
-	end
-	for index = 1, #notSideMesh, 3 do
-		local a = notSideMesh[index]
-		local b = notSideMesh[index+1]
-		local c = notSideMesh[index+2]
-		local aPos = matrix * a.pos
-		local bPos = matrix * b.pos
-		local cPos = matrix * c.pos
-		debugoverlay.Triangle(aPos,bPos,cPos,20,Color(0,255,0),true)
-		debugoverlay.Line(aPos, bPos, 20, Color(0,0,0),true)
-		debugoverlay.Line(bPos, cPos, 20, Color(0,0,0),true)
-		debugoverlay.Line(cPos, aPos, 20, Color(0,0,0),true)
 	end
 	return sideMesh, notSideMesh
 end
 
-
-function SWEP:PrimaryAttack()
-	if (game.SinglePlayer()) then 
-		self:CallOnClient("PrimaryAttack")
+function SplitMultiMesh(meshes, matrix, planeDirection, planeDistance)
+	local sideMeshes, notSideMeshes = {}, {}
+	for index = 1, #meshes do
+		local mesh = meshes[index]
+		local sideMesh = Mesh()
+		local notSideMesh = Mesh()
+		local sideTriangles, notSideTriangles = SplitMesh(mesh.originalTriangles or mesh.triangles, matrix, planeDirection, planeDistance)
+		sideMesh:BuildFromTriangles(sideTriangles)
+		notSideMesh:BuildFromTriangles(notSideTriangles)
+		table.insert(sideMeshes, {originalTriangles = sideTriangles, triangles = sideMesh, material = mesh.material})
+		table.insert(notSideMeshes, {originalTriangles = notSideTriangles, triangles = notSideMesh, material = mesh.material})
 	end
-	if (!CLIENT) then 
-		if (self.Owner:IsPlayer()) then
-			self.Owner:LagCompensation(true)
-		end
-		local startpos = self.Owner:GetShootPos()
-		local endpos = self.Owner:GetShootPos() + self.Owner:GetAimVector() * 100
-		local trace = util.TraceLine({
-			start = startpos,
-			endpos = endpos,
-			filter = self.Owner
-		})
-		if (self.Owner:IsPlayer()) then
-			self.Owner:LagCompensation(false)
-		end
-		if (IsValid(trace.Entity)) then
-			local planePosition = trace.HitPos
-			local planeDirection = self.Owner:GetAngles():Right()
-			local planeDistance = GetPlaneDistance(planeDirection, planePosition)
-			self:SetTPlanePosition(planePosition)
-			self:SetTPlaneDistance(planeDistance)
-			local physicsObject = trace.Entity:GetPhysicsObject()
-			local matrix = physicsObject:GetPositionMatrix()
-			local mesh = physicsObject:GetMesh()
-			if (mesh) then
-				local sideMesh, notSideMesh = SplitMesh(mesh, matrix, planeDirection, planeDistance)
-				local copied = duplicator.Copy(trace.Entity)
-				local pastedEntities, pastedConstraints = duplicator.Paste(self.Owner, copied.Entities, copied.Constraints)
-				trace.Entity:PhysicsFromMesh(sideMesh)
-				local firstKey, firstValue = next(pastedEntities)
-				firstValue:PhysicsFromMesh(notSideMesh)
-				self:SetSlicedIndexA(trace.Entity:EntIndex())
-				self:SetSlicedIndexB(firstValue:EntIndex())
+	return sideMeshes, notSideMeshes
+end
+
+function Duplicate(entity, owner)
+	local copied = duplicator.Copy(entity)
+	local pastedEntities, pastedConstraints = duplicator.Paste(owner, copied.Entities, copied.Constraints)
+	local firstKey, firstValue = next(pastedEntities)
+	return firstValue
+end
+
+function RenderSlice(entity)
+	if (entity.meshes) then
+		cam.PushModelMatrix(entity:GetWorldTransformMatrix())
+		for index = 1, #entity.meshes do
+			local mesh = entity.meshes[index]
+			local material
+			if (cachedMaterials[mesh.material]) then
+				material = cachedMaterials[mesh.material]
+			else
+				material = Material(mesh.material)
+				cachedMaterials[mesh.material] = material
 			end
+			render.SetMaterial(material)
+			mesh.triangles:Draw()
 		end
-	else
-		local side = ents.GetByIndex(self:GetSlicedIndexA())
-		local notSide = ents.GetByIndex(self:GetSlicedIndexB())
-		if (side && notSide) then
-			local planePosition = self:GetTPlanePosition()
-			local planeDistance = self:GetTPlaneDistance()
-			local matrix = side:GetWorldTransformMatrix()
-			--TODO: GET CLIENT-SIDE MESH, WAITING THIS FEATURE FROM GMOD :C
-		end
+		entity:CreateShadow()
+		cam.PopModelMatrix()
 	end
-end
-
-function SWEP:SecondaryAttack()
-
-end
-
-function SWEP:OnRemove()
-
-
-end
-
-function SWEP:Holster()
-	return true
-end
-
-function SWEP:CustomAmmoDisplay()
-	return self.AmmoDisplay
 end
